9b92bcb6f72eecf331fc8fcedea71732aeaa2466
Sean <seanreilly123@hotmail.com>
2020-04-27 17:32:21 +1200

    Path to original

--- src/App.js
+++ src/App.js
@@ -11,17 +11,11 @@ function App() {
         <Router>
             <div className="App">
                 <Switch>
-                    <Route path="/react-restaurant/" component={Home} exact />
-                    <Route path="/react-restaurant/menu" component={MenuPage} />
-                    <Route
-                        path="/react-restaurant/locations"
-                        component={LocationsList}
-                    />
-                    <Route path="/react-restaurant/team" component={TeamList} />
-                    <Route
-                        path="/react-restaurant/contact"
-                        component={ContactPage}
-                    />
+                    <Route path="/" component={Home} exact />
+                    <Route path="/menu" component={MenuPage} />
+                    <Route path="/locations" component={LocationsList} />
+                    <Route path="/team" component={TeamList} />
+                    <Route path="/contact" component={ContactPage} />
                 </Switch>
             </div>
         </Router>
