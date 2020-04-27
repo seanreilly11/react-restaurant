9ce3bdf2fc94a94d83237e67aa4b5e40247b0b9b
Sean <seanreilly123@hotmail.com>
2020-04-27 16:08:31 +1200

    Updates

---
+++ precache-manifest.a6ee265256f63464033768d80f0e542d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53017f1ebd6d360624151fcecbec884",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "d4797054f13b46b959e3",
+    "url": "/react-restaurant/static/css/main.497827b2.chunk.css"
+  },
+  {
+    "revision": "1b6ab1f164aa9fcbecd4",
+    "url": "/react-restaurant/static/js/2.454e8192.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-restaurant/static/js/2.454e8192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4797054f13b46b959e3",
+    "url": "/react-restaurant/static/js/main.ce1887d7.chunk.js"
+  },
+  {
+    "revision": "846ae3ca4fd971c37565",
+    "url": "/react-restaurant/static/js/runtime-main.1edc9eee.js"
+  },
+  {
+    "revision": "a27eb74b01ee3aa46f25de2b81faeff2",
+    "url": "/react-restaurant/static/media/akl.a27eb74b.jpg"
+  },
+  {
+    "revision": "ace9d0fbd2d0b47d7ce8c26562c95eb0",
+    "url": "/react-restaurant/static/media/banner.ace9d0fb.jpg"
+  },
+  {
+    "revision": "e45bf5ce823535edb3a7cf0257a96dc9",
+    "url": "/react-restaurant/static/media/brekkie.e45bf5ce.jpg"
+  },
+  {
+    "revision": "6fce32959581e7614fc8fbc3e6869b16",
+    "url": "/react-restaurant/static/media/chch.6fce3295.jpg"
+  },
+  {
+    "revision": "77ffb6b806a5273702ecc5e97e6f03d3",
+    "url": "/react-restaurant/static/media/dinner.77ffb6b8.jpg"
+  },
+  {
+    "revision": "4a5000fa59cb743bc586678eff9c4cbf",
+    "url": "/react-restaurant/static/media/female1.4a5000fa.jpg"
+  },
+  {
+    "revision": "a9b68f88a2c7d1fa70739ab4d0604999",
+    "url": "/react-restaurant/static/media/female2.a9b68f88.jpg"
+  },
+  {
+    "revision": "e2e167e2ab6b2cac4a7c8c5b08ce8bda",
+    "url": "/react-restaurant/static/media/female3.e2e167e2.jpg"
+  },
+  {
+    "revision": "c050b5deb7cb716211f5878f934e36bc",
+    "url": "/react-restaurant/static/media/female4.c050b5de.jpg"
+  },
+  {
+    "revision": "de7d1a405bab2d31411497d873d30003",
+    "url": "/react-restaurant/static/media/female5.de7d1a40.jpg"
+  },
+  {
+    "revision": "343ecaf8a4bbcf55cc985925009c71b0",
+    "url": "/react-restaurant/static/media/lunch.343ecaf8.jpg"
+  },
+  {
+    "revision": "ba73b3c87580d99f525452feadf95099",
+    "url": "/react-restaurant/static/media/male1.ba73b3c8.jpg"
+  },
+  {
+    "revision": "dc4df77d7d729b4fff7787be25bb2be0",
+    "url": "/react-restaurant/static/media/male2.dc4df77d.jpg"
+  },
+  {
+    "revision": "a554e55931f075f4d35491cf35366acb",
+    "url": "/react-restaurant/static/media/male3.a554e559.jpg"
+  },
+  {
+    "revision": "d9c22651df8d73fc3c47207060c3a46f",
+    "url": "/react-restaurant/static/media/male4.d9c22651.jpg"
+  },
+  {
+    "revision": "31646e978b3d90ef2ec7616d117fdebe",
+    "url": "/react-restaurant/static/media/upperhutt.31646e97.jpg"
+  },
+  {
+    "revision": "b9c1fdfa050d68ccc5d77308a3b234bc",
+    "url": "/react-restaurant/static/media/welly.b9c1fdfa.jpg"
+  }
+]);
